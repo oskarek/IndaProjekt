9eff689e1ee12327cc1767889cab1d8e9a7df77f
Oskar Ek <oskarek24@gmail.com>
2015-05-11 22:30:03 +0200

    Förbättrat HichscoreTool

--- src/HighscoreTool.java
+++ src/HighscoreTool.java
@@ -1,6 +1,5 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 
 /**
  * A class with various tools for highscores.
@@ -8,25 +7,49 @@ import java.io.IOException;
  * Created by oskarek on 2015-05-11.
  */
 public class HighscoreTool {
-    private static final String FILEPATH = "res/highscores/highscores.txt";
+    private static final String FILENAME = "res/highscores/highscores.txt";
+    private static final String FILEPATH = "res/highscores/";
 
 
-    public boolean isHighscore(int score) {
-        try(BufferedReader file = new BufferedReader(new FileReader(FILEPATH));) {
+    public void insertScore(int score, String name) {
+        ArrayList<String> entries = createNewOrder(score, name);
+        updateFile(entries);
+    }
+
+    private ArrayList<String> createNewOrder(int score, String name) {
+        ArrayList<String> newOrder = new ArrayList<>();
+        try(BufferedReader file = new BufferedReader(new FileReader(FILENAME))) {
             for (String currentLine=file.readLine(); currentLine!=null; currentLine=file.readLine()) {
-                int currentScore = Integer.parseInt(currentLine.split("\\s+")[1]);
+                int currentScore = Integer.parseInt(currentLine.split("\\s+")[0]);
                 if (currentScore <= score) {
-                    return true;
+                    newOrder.add(score + " " + name);
                 }
+                newOrder.add(currentLine);
+            }
+            if (newOrder.isEmpty()) {
+                newOrder.add(score + " " + name);
+            }
+            if (newOrder.size()>10) {
+                newOrder.remove(10);
             }
         } catch (IOException e) {
             System.err.println("Failed to read the highscore file.");
         }
-
-        return false;
+        return newOrder;
     }
 
-    public void insertHighscore(int score) {
-
+    private void updateFile(ArrayList<String> entries) {
+        System.out.println(entries.size());
+        try(FileWriter writer = new FileWriter(FILENAME)) {
+            for (int i = 0; i < entries.size(); i++) {
+                String entry = entries.get(i);
+                writer.write(entry);
+                if (i != entries.size()-1) {
+                    writer.write("\n");
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Failed to write the highscore file.");
+        }
     }
 }
