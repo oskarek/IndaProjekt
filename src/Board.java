799c66b80f41cee8a0457a7d42cef1449fb929e9
Robert Lorentz <robey112@live.se>
2015-05-11 16:24:28 +0200

    Highscore + Powerups screen klasser gjorda. Back knapp.

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,5 @@
+import org.newdawn.slick.*;
+
 /**
  * SKELETON CODE
  * The board that you control in the game.
@@ -7,12 +9,34 @@
 public class Board extends PlayingFieldItem {
     private PowerUp currentPowerUp;
     private int length;
+    private Image boardImage;
+    private int xPos;
+
+    public Board() throws SlickException {
+        boardImage = new Image("res/UIButtons/board.png");
+        length = boardImage.getWidth();
+    };
 
     public int getLength(){
         return length;
     }
-
     public void setLength(int length){
         this.length = length;
+    }
+
+    public int getxPos(){
+        return xPos;
+    }
+
+    public void setxPos(int xPos){
+        this.xPos = xPos;
+    }
+
+    public Image getBoardImage(){
+        return boardImage;
+    }
+
+    public void setBoardSize(Image image){
+        boardImage = image;
     }
 }
--- src/PlayingField.java
+++ src/PlayingField.java
@@ -9,7 +9,10 @@ import org.newdawn.slick.state.StateBasedGame;
  * Created by oskarek on 2015-05-07.
  */
 public class PlayingField extends BasicGameState {
-
+    private int contWidth, contHeight;
+    private static final int DIRECTION_LEFT = -1;
+    private static final int DIRECTION_RIGHT = 1;
+    Board board;
     @Override
     public int getID() {
         return 1;
@@ -17,6 +20,11 @@ public class PlayingField extends BasicGameState {
 
     @Override
     public void init(GameContainer container, StateBasedGame game) throws SlickException {
+        // dimensions of the container
+        board = new Board();
+        contHeight = container.getHeight();
+        contWidth = container.getWidth();
+
 
     }
 
@@ -28,17 +36,55 @@ public class PlayingField extends BasicGameState {
 
     }
 
-    public void createBoard() throws SlickException {
+    private void updateBallPos(Graphics g)throws SlickException {
 
     }
 
+    public void drawBoard(Graphics g) throws SlickException {
+        Image boardImage = board.getBoardImage();
+        int xPos = board.getxPos();
+        int yPos = contHeight-30;
+        g.drawImage(boardImage, xPos, yPos);
+    }
+
+    private void updateBoardPos(int direction) throws SlickException {
+        if(direction == DIRECTION_LEFT){
+            int currentxPos = board.getxPos();
+            if(currentxPos > 0) {
+                int xPos = currentxPos - 5;
+                board.setxPos(xPos);
+            }
+        }
+        if(direction == DIRECTION_RIGHT){
+            int boardLength = board.getLength();
+            int currentxPos = board.getxPos();
+            if(currentxPos+boardLength < contWidth) {
+                int xPos;
+                if(currentxPos+5 >= contWidth){
+                    xPos = contWidth-boardLength;
+                } else {
+                    xPos = currentxPos + 5;
+
+                }
+                board.setxPos(xPos);
+            }
+        }
+    }
+
+
     @Override
     public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
-
+        drawBoard(g);
     }
 
     @Override
     public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
-
+        Input input = container.getInput();
+        if (input.isKeyDown(Input.KEY_LEFT)) {
+            updateBoardPos(DIRECTION_LEFT);
+        }
+        if (input.isKeyDown(Input.KEY_RIGHT)) {
+            updateBoardPos(DIRECTION_RIGHT);
+        }
     }
 }
